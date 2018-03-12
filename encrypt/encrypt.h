21ee221da7a9020aa73580c00ca0d5d00ddd6382
garfield <shaudai0402@hotmail.com>
2018-03-12 11:43:58 +0800

    Add more comments in header file

--- encrypt/encrypt.h
+++ encrypt/encrypt.h
@@ -9,10 +9,36 @@
 
 // Use mbedTLS.lib to implement encryption function in encrypt.dll
 
-// Implement AES-256-CBC encryption
+/**
+* \brief          Implement AES-256-CBC encryption
+*                 Refer to the 'mbedtls_aes_*' APIs in
+*                 '\mbedtls\library\aes.c'
+*                 '\mbedtls\include\mbedtls\aes.h'
+*
+* \param minutiae            Pointer to the minutiae binary with 512 bytes length
+* \param iv                  Initial vector for AES-256-CBC algorithm
+* \param key                 Pointer to AES-256 key (key length: 256 bits) 
+* \param encrypted_minutiae  Pointer to the encrypted minutiae
+*
+* \return         0 on success, or 1 on failure.
+*/
 extern "C" ENCRYPT_EXPORTS int gen_aes_encrypted_minutiae(const unsigned char *minutiae,
 	const unsigned char *iv, const unsigned char *key, unsigned char *encrypted_minutiae);
 
-// Implement RSA encryption
+/**
+* \brief          Implement RSA encryption
+*                 Refer to
+*                 '\mbedtls\programs\pkey\rsa_encrypt.c' - for RSAES-PKCS1-v1_5 operation
+*                 '\mbedtls\programs\pkey\pk_encrypt.c'  - for extracting rsa context from PEM-formatted public key
+*
+* \param session_key            Pointer to AES-256 key (key length: 256 bits)
+* \param rsa_key_path           Path to RSA public key file
+*                               PEM format,
+*                               starts with '-----BEGIN PUBLIC KEY-----',
+*                               ends with '-----END PUBLIC KEY-----'
+* \param encrypted_session_key  Pointer to encryption result
+*
+* \return         0 on success, or 1 on failure.
+*/
 extern "C" ENCRYPT_EXPORTS int gen_rsa_encrypted_skey(const unsigned char *session_key, 
 	const char *rsa_key_path, unsigned char *encrypted_session_key);
