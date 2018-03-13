750b597b33c415f53f5aa381a468c013be6dd81d
garfield <shaudai0402@hotmail.com>
2018-03-13 10:15:49 +0800

    [node client for Test] use request() to POST encrypted minutiae, session key, and initial vector to /api/addFP

--- node/client/client.js
+++ node/client/client.js
@@ -1,5 +1,6 @@
 const ffi = require('ffi');
 const ref = require('ref');
+const request = require('request');
 
 const encryptConfig = require('./config');
 
@@ -25,3 +26,18 @@ console.log('\nencryptedMinutiae:\n', encryptedMinutiae.toString('hex'));
 
 encryptDll.gen_rsa_encrypted_skey(encryptConfig.key, encryptConfig.keyPath, encryptedSkey);
 console.log('\nencryptedSkey:\n', encryptedSkey.toString('hex'));
+
+request.post( {
+  url: `${encryptConfig.addFpUrl}`,
+  form: {
+    fpIndex: `${encryptConfig.fpIndex}`,
+    encMinutiae: `${encryptedMinutiae.toString('hex')}`,
+    userId: `${encryptConfig.userId}`,
+    eSkey: `${encryptedSkey.toString('hex')}`,
+    iv: `${encryptConfig.iv.toString('hex')}`
+  },
+}, (err, res, body) => {
+  console.log('error: ', err);
+  console.log('statusCode: ', res && res.statusCode);
+  console.log('body: ', body);
+});
